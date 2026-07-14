bcf2629bef6f99734622a86652f905a41f22f61b
iomatix <mateuszwypchlak@gmail.com>
2026-07-14 16:34:56 +0200

    important fix

--- LabApi.Extensions/Misc/iLogger.cs
+++ LabApi.Extensions/Misc/iLogger.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace LabApi.Extensions.Misc
 {
@@ -17,6 +18,7 @@ namespace LabApi.Extensions.Misc
         /// <summary>
         /// Automatically resolves and caches the full dotted call path (Namespace.Class.Method) of the caller.
         /// </summary>
+        [MethodImpl(MethodImplOptions.NoInlining)]
         private static string GetDefaultSource()
         {
             // Skip 2 frames: Frame 0 is GetDefaultSource, Frame 1 is the iLogger method (Info/Warn), Frame 2 is the actual plugin method!
